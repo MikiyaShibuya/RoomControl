bb5862c8880ce8c7b3174d9a82f1fcc3228a1393
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2022-05-08 02:12:48 +0900

    Add aircon controller

--- aircon.py
+++ aircon.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python3
 
 import sys
+import subprocess
 
-print(sys.argv)
+def get():
+    print("0")
+    return 0
 
-with open("/home/pi/RoomControl/test.txt", mode='a') as f:
-    f.write(str(sys.argv))
-    f.write('\n')
+
+def set():
+
+    key = sys.argv[3]
+    value = sys.argv[4]
+
+    if key == 'Active' and value == '1':
+        subprocess.run(['python3', '/home/pi/RoomControl/irrp.py', '-p', '-g17', '-f', '/home/pi/RoomControl/codes', 'aircon:auto'])
+    elif key == 'Active' and value == '0':
+        subprocess.run(['python3', '/home/pi/RoomControl/irrp.py', '-p', '-g17', '-f', '/home/pi/RoomControl/codes', 'aircon:off'])
+    else:
+        subprocess.run(['python3', '/home/pi/RoomControl/irrp.py', '-p', '-g17', '-f', '/home/pi/RoomControl/codes', 'aircon:auto'])
+
+
+
+if __name__ == "__main__":
+
+    # with open("/home/pi/RoomControl/test.txt", mode='a') as f:
+    #     f.write('aaa')
+
+    act = sys.argv[1]
+    tgt = sys.argv[2]
+
+    if act == 'Get':
+        get()
+    elif act == 'Set':
+        set()
+
+        
+
