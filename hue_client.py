934c34ddb6eeda96e71730eee1786f70fba869ac
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2022-05-08 01:27:01 +0900

    Update hue controller

--- hue_client.py
+++ hue_client.py
@@ -1,38 +1,72 @@
 #!/usr/bin/env python3
 import requests
 import argparse
+import sys
+import json
 
 
-def switch(base_url, bri):
+def on_off(base_url, to_on):
+    api_url = base_url + 'action'
+    payload = {'on': to_on}
+
+    print(api_url, payload)
+
+    res = requests.put(api_url, json=payload)
+
+
+def set_brightness(base_url, bri):
     api_url = base_url + 'action'
     brightness = int(max(0, min(254, bri * 254)))
     to_on = brightness != 0
     payload = {'on': to_on, 'bri': brightness}
 
+    print(api_url, payload)
+
     res = requests.put(api_url, json=payload)
+    print(res)
 
 
-def main(hue_user, bri):
+def get(base_url):
+    payload = {}
+    res = requests.get(base_url, json=payload)
+    data = json.loads(res.text)
 
-    base_url = f'http://192.168.11.3/api/{hue_user}/groups/1/'
-    switch(base_url, bri)
+    key = sys.argv[4]
+    if key == "On": 
+        print(data['action']['on'])
+    elif key == "Brightness":
+        bri = int(data['action']['bri'])
+        print(int(bri / 254.0 * 100))
+
+    return 0
+
+
+def set(base_url):
+
+    key = sys.argv[4]
+    value = sys.argv[5]
+
+    if key == "On" and value == "0":
+        on_off(base_url, False)
+    elif key == "On" and value == "1":
+        on_off(base_url, True)
+    elif key == "Brightness":
+        set_brightness(base_url, int(value) / 100.0)
+    else:
+        print(f"unknown key: {key} - {value}")
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='python hue control script')
-    parser.add_argument('-u', type=str, required=True,
-                        help='User name for HUE api')
-    parser.add_argument('-b', type=int, required=True,
-                        help='brightness (0-100)')
-
-    args = parser.parse_args()
-
-    hue_user = ''
-
-    with open(args.u) as f:
+    with open(sys.argv[1]) as f:
         hue_user = f.read().splitlines()[0]
+    base_url = f'http://192.168.11.3/api/{hue_user}/groups/1/'
 
-    main(hue_user, args.b / 100.0)
+    act = sys.argv[2]
+    tgt = sys.argv[3]
 
+    if act == 'Get':
+        get(base_url)
+    elif act == 'Set':
+        set(base_url)
 
